2d575a224e10922d62ca0155fbcdf8b2707c821f
danicormu <danicormu@gmail.com>
2016-06-07 08:29:20 -0600

    First Commit
    Este commit contiene el método de FizzBuzz hecho de la forma eficiente.

---
+++ src/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fizzbuzz;
+
+/**
+ *
+ * @author Daniel
+ */
+public class FizzBuzz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i =1; i <= 100; i++){
+            if(i % 15 == 0){
+                System.out.println("FizzBuzz");
+            }else if(i % 5 == 0){
+                System.out.println("Buzz");
+            }else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }else
+                System.out.println(i);
+            
+        }
+    }  
+}
